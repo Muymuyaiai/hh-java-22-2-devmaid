02086f301d6760fd95dd90374f5c7e3d4f6a626e
leonmaar <myu@gmx.net>
2022-10-24 15:16:15 +0200

    Backend POST-Request for code translation: controller, service and models

---
+++ backend/src/main/java/com/example/backend/model/Gpt3Request.java
@@ -0,0 +1,24 @@
+package com.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Gpt3Request {
+    private String model;
+    private String prompt;
+    private float temperature;
+    private int max_tokens;
+    private float top_p;
+    private float frequency_penalty;
+    private float presence_penalty;
+    private List stop;
+}
